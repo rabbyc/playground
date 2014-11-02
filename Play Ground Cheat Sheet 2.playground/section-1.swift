8cb262c84f9020c827f3cd7b6ef48a198b35f53b
rabbyph@gmail.com <rabby.calicdan@gmail.com>
2014-11-02 12:30:58 +0800

    updated with loops

--- Play Ground Cheat Sheet 2.playground/section-1.swift
+++ Play Ground Cheat Sheet 2.playground/section-1.swift
@@ -13,6 +13,10 @@ var num:Int = 9999
 //Concatenating Text
 println("There are no Strings on me. By \(text) Power Level \(num)")
 
+//Just a spacer for the console output
+println()
+
+
 //Declaring Constant Variables (fixed value, Var name cannot be reused)
 let multiplier:Int = 25
 let multiply:Int = 4
@@ -20,6 +24,28 @@ let multiply:Int = 4
 //Using Constant Variables in Operations
 println("Four 25 Cents is equal to \((multiplier * multiply)/100) Dollar")
 
+//Just a spacer for the console output
+println()
+
 //Arrays, User of Brackets []
 
+var loopVariable = ["A", "B", "C", "D", "E"]
+var getVarialbleFromIndex = loopVariable[0]
+
 //Loops, While condition is still true
+
+//For
+
+for letters in loopVariable{
+    println(letters)
+}
+
+//Just a spacer for the console output
+println()
+
+
+for (var i = 0; i < 3; ++i){
+    println(loopVariable[i])
+}
+
+//Conditional Statements
