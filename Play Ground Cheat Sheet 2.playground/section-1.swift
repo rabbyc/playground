849d5fa12e30676f42725b347026097dbb53d561
rabbyph@gmail.com <rabby.calicdan@gmail.com>
2014-11-02 10:51:43 +0800

    first commit

---
+++ Play Ground Cheat Sheet 2.playground/section-1.swift
@@ -0,0 +1,25 @@
+// Playground - noun: a place where people can play
+// In Git go to desired folder and execute git Init to change the terminal git
+
+import UIKit
+
+//Declaring Variables
+var str = "Hello, playground"
+
+//Declaring Strong Variable Types
+var text:String = "Ultron"
+var num:Int = 9999
+
+//Concatenating Text
+println("There are no Strings on me. By \(text) Power Level \(num)")
+
+//Declaring Constant Variables (fixed value, Var name cannot be reused)
+let multiplier:Int = 25
+let multiply:Int = 4
+
+//Using Constant Variables in Operations
+println("Four 25 Cents is equal to \((multiplier * multiply)/100) Dollar")
+
+//Arrays, User of Brackets []
+
+//Loops, While condition is still true
